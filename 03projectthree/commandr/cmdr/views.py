dfea6840d48fbc0c167ad7e4421cfa710cb29046
Amit Warbhe <amitwarbhe@gmail.com>
2022-08-19 12:50:09 +0530

    first commit

---
+++ 03projectthree/commandr/cmdr/views.py
@@ -0,0 +1,7 @@
+from msilib.schema import ListView
+from django.views.generic import ListView
+from .models import Cmdr
+
+class HomePageView(ListView):
+    model = Cmdr
+    template_name = 'home.html'
